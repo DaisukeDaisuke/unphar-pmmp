9243c4475248fc459b4d497af8f550c9c82d67a9
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2021-07-28 20:50:51 +0900

    support pmmp api 4

--- src/unphar/unphar.php
+++ src/unphar/unphar.php
@@ -1,48 +1,49 @@
 <?php
 namespace unphar;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\Player;
-use pocketmine\Server;
-
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\command\ConsoleCommandSender;
+use pocketmine\console\ConsoleCommandSender as pm4ConsoleSender;
+use pocketmine\command\ConsoleCommandSender as pm3ConsoleSender;
 
-class unphar extends unpharcommand implements Listener{
+class unphar extends unpharcommand{
 
-	public function onEnable(){
+	public function onEnable() : void{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		//for api 2.0
 		if(!file_exists($this->getDataFolder()))
-		mkdir($this->getDataFolder(), 0744, true);
-		
+		mkdir($this->getDataFolder(), 0755, true);
+
 		if(!file_exists($this->getDataFolder()."target".DIRECTORY_SEPARATOR))
-		mkdir($this->getDataFolder()."target".DIRECTORY_SEPARATOR, 0744, true);
+		mkdir($this->getDataFolder()."target".DIRECTORY_SEPARATOR, 0755, true);
 		
 		if(!file_exists($this->getDataFolder()."output".DIRECTORY_SEPARATOR))
-		mkdir($this->getDataFolder()."output".DIRECTORY_SEPARATOR, 0744, true);
+		mkdir($this->getDataFolder()."output".DIRECTORY_SEPARATOR, 0755, true);
 	}
 
-	public function _onCommand(CommandSender $sender, Command $command, $label, array $args){
+	public function _onCommand(CommandSender $sender, Command $command, $label, array $args) : bool{
 		switch(strtolower($label)){
 			case "unphar":
-				if($sender instanceof ConsoleCommandSender){
+				if($sender instanceof pm3ConsoleSender||$sender instanceof pm4ConsoleSender){
 					$this->getLogger()->info("unphar - start");
-					$this->unphar($this->getDataFolder()."target".DIRECTORY_SEPARATOR);
+					$this->onunphar($this->getDataFolder()."target".DIRECTORY_SEPARATOR);
 					$this->getLogger()->info("unphar - exit.");
 					return true;
-				}else{
-					if($sender->isOP())
-					$sender->sendMessage("コンソールから使用しましょう。");
-					return true;
 				}
-			break;
+
+				if($this->isOP($sender)){
+					$sender->sendMessage("Please use from the console.");
+				}
+				return true;
 		}
 		return true;
 	}
 
-	public function unphar($target){
+	public function isOp(CommandSender $sender) : bool{
+		return $this->getServer()->isOp($sender->getName());//for api4.0
+	}
+
+	public function onunphar($target){
 		$slash = DIRECTORY_SEPARATOR;
 		foreach(new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($target)) as $path => $file){
 			if($file->isFile() === false){
@@ -59,18 +60,18 @@ class unphar extends unpharcommand implements Listener{
 	public function extractphar($targetfile,$path,$slash){
 		if(is_dir($targetfile) && $handle = opendir($targetfile)){
 			while(($file = readdir($handle)) !== false){
-				if(($type = filetype($target = $targetfile.$file)) == "file"){
+				if(($type = filetype($target = $targetfile.$file)) === "file"){
 					$filename = basename($path,".phar");
 					$subpath = substr($target, strlen("phar://".$path.$slash));
 					$output = $this->getDataFolder()."output".$slash.$filename.$slash.$subpath;
 					if(!file_exists(dirname($output))){
-						mkdir(dirname($output), 0744, true);
+						mkdir(dirname($output), 0755, true);
 					}
 					if(!copy($target,$output)){
 						$this->getLogger()->info("error 展開が出来ませんでした... $target --> $output");
 					}
 				}else{
-					if($type == "dir"){
+					if($type === "dir"){
 						$this->extractphar($target.$slash,$path,$slash);
 					}
 				}
--- src/unphar/unpharcommand.php
+++ src/unphar/unpharcommand.php
@@ -6,17 +6,17 @@ use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 
-
-if(strpos((new \ReflectionClass('pocketmine\plugin\PluginBase'))->getMethod("onCommand")->getParameters()[2],"string")){
+//support pmmp api 2.0
+if(strpos((new \ReflectionClass(PluginBase::class))->getMethod("onCommand")->getParameters()[2],"string")){
 	abstract class unpharcommand extends PluginBase implements Listener{
-		abstract public function _onCommand(CommandSender $sender, Command $command, $label, array $args);
+		abstract public function _onCommand(CommandSender $sender, Command $command, $label, array $args) : bool;
 		public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 			return $this->_onCommand($sender, $command, $label, $args);
 		}
 	}
 }else{
 	abstract class unpharcommand extends PluginBase implements Listener{
-		abstract public function _onCommand(CommandSender $sender, Command $command, $label, array $args);
+		abstract public function _onCommand(CommandSender $sender, Command $command, $label, array $args) : bool;
 		public function onCommand(CommandSender $sender, Command $command, $label, array $args){
 			return $this->_onCommand($sender, $command, $label, $args);
 		}
