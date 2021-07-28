855db4d264a661442da6e2865e000dcfff80156d
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2021-07-29 08:38:42 +0900

    drop support pmmp api 2.0, 4.0

--- src/DaisukeDaisuke/unphar/Main.php
+++ src/DaisukeDaisuke/unphar/Main.php
@@ -1,45 +1,37 @@
 <?php
+
 namespace DaisukeDaisuke\unphar;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\console\ConsoleCommandSender as pm4ConsoleSender;
-use pocketmine\command\ConsoleCommandSender as pm3ConsoleSender;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\plugin\PluginBase;
 
-class unphar extends unpharcommand{
-
+class Main extends PluginBase{
 	public function onEnable() : void{
-		//for api 2.0
-		if(!file_exists($this->getDataFolder()))
-		mkdir($this->getDataFolder(), 0755, true);
-
 		if(!file_exists($this->getDataFolder()."target".DIRECTORY_SEPARATOR))
-		mkdir($this->getDataFolder()."target".DIRECTORY_SEPARATOR, 0755, true);
-		
+			mkdir($this->getDataFolder()."target".DIRECTORY_SEPARATOR, 0755);
+
 		if(!file_exists($this->getDataFolder()."output".DIRECTORY_SEPARATOR))
-		mkdir($this->getDataFolder()."output".DIRECTORY_SEPARATOR, 0755, true);
+			mkdir($this->getDataFolder()."output".DIRECTORY_SEPARATOR, 0755);
 	}
 
-	public function _onCommand(CommandSender $sender, Command $command, $label, array $args) : bool{
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 		switch(strtolower($label)){
 			case "unphar":
-				if($sender instanceof pm3ConsoleSender||$sender instanceof pm4ConsoleSender){
+				if($sender instanceof ConsoleCommandSender){
 					$this->getLogger()->info("unphar - start");
 					$this->onunphar($this->getDataFolder()."target".DIRECTORY_SEPARATOR);
 					$this->getLogger()->info("unphar - exit.");
 					return true;
 				}
 
-				if($this->isOP($sender)){
+				if($sender->isOp()){
 					$sender->sendMessage("Please use from the console.");
 				}
 				return true;
 		}
 		return true;
-	}
-
-	public function isOp(CommandSender $sender) : bool{
-		return $this->getServer()->isOp($sender->getName());//for api4.0
 	}
 
 	public function onunphar($target){
@@ -48,30 +40,30 @@ class unphar extends unpharcommand{
 			if($file->isFile() === false){
 				continue;
 			}
-			if(substr($path, strrpos($path, '.')+1) === "phar"){
-				$this->getLogger()->info("unphar - ".str_replace($this->getDataFolder()."target".$slash,'',$path));
+			if(substr($path, strrpos($path, '.') + 1) === "phar"){
+				$this->getLogger()->info("unphar - ".str_replace($this->getDataFolder()."target".$slash, '', $path));
 				$pharPath = "phar://".$path.$slash;
-				$this->extractphar($pharPath,$path,$slash);
+				$this->extractphar($pharPath, $path, $slash);
 			}
 		}
 	}
 
-	public function extractphar($targetfile,$path,$slash){
-		if(is_dir($targetfile) && $handle = opendir($targetfile)){
+	public function extractphar($targetfile, $path, $slash){
+		if(is_dir($targetfile)&&$handle = opendir($targetfile)){
 			while(($file = readdir($handle)) !== false){
 				if(($type = filetype($target = $targetfile.$file)) === "file"){
-					$filename = basename($path,".phar");
+					$filename = basename($path, ".phar");
 					$subpath = substr($target, strlen("phar://".$path.$slash));
 					$output = $this->getDataFolder()."output".$slash.$filename.$slash.$subpath;
 					if(!file_exists(dirname($output))){
 						mkdir(dirname($output), 0755, true);
 					}
-					if(!copy($target,$output)){
+					if(!copy($target, $output)){
 						$this->getLogger()->info("error 展開が出来ませんでした... $target --> $output");
 					}
 				}else{
 					if($type === "dir"){
-						$this->extractphar($target.$slash,$path,$slash);
+						$this->extractphar($target.$slash, $path, $slash);
 					}
 				}
 			}
